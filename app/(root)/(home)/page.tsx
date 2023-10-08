3cf9a70844244e07788e80560b02cfac6e2729ea
Adilson Oliveira <adilsonreisoliveira.98@gmail.com>
2023-10-08 09:43:56 -0300

    feat: introduce a auth method

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div className="flex-center">
-      <h1 className="h1-bold">Next.js we&rsquo;re coming</h1>
+    <div>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
